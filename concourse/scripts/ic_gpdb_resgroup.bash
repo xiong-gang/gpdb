624894b05f6efcc565d17c2f18782b4b1697ed83
Gang Xiong <gxiong@pivotal.io>
2017-07-11 14:37:56 +0800

    vm

--- concourse/scripts/ic_gpdb_resgroup.bash
+++ concourse/scripts/ic_gpdb_resgroup.bash
@@ -2,27 +2,45 @@
 
 set -eox pipefail
 
-CWDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+./ccp_src/aws/setup_ssh_to_cluster.sh
 
-basedir=/sys/fs/cgroup
-options=rw,nosuid,nodev,noexec,relatime
-groups="hugetlb freezer pids devices cpuset blkio net_prio net_cls cpuacct cpu memory perf_event"
+CLUSTER_NAME=$(cat ./cluster_env_files/terraform/name)
 
-# mount cgroup base dir
-mkdir -p $basedir
-mount -t tmpfs tmpfs $basedir
+prepare_cgroups() {
+    local gpdb_host_alias=$1
+    # Requires the git Concourse resource to make the source available
 
-# mount cgroup controllers
-for group in $groups; do
-	mkdir -p $basedir/$group
-	mount -t cgroup -o $options,$group cgroup $basedir/$group
-done
+    local basedir=/cgroup
+    local options=rw,nosuid,nodev,noexec,relatime
+    local groups="hugetlb freezer pids devices cpuset blkio net_prio net_cls cpuacct cpu memory perf_event"
 
-mkdir -p $basedir/cpu/gpdb
+    ssh -t $gpdb_host_alias "sudo bash -c '(\
+        mkdir -p $basedir; \
+        mount -t tmpfs tmpfs $basedir; \
+        for group in $groups; do \
+                mkdir -p $basedir/\$group; \
+                mount -t cgroup -o $options,\$group cgroup $basedir/\$group; \
+        done; \
+        mkdir -p $basedir/cpu/gpdb; \
+        find $basedir -type d | xargs chmod 777; \
+    )'"
+}
 
-# set all dirs' permission to 777 to allow test cases to control
-# when and how cgroup is enabled
-find $basedir -type d | xargs chmod 777
+run_resgroup_test() {
+    local gpdb_master_alias=$1
+    # Requires the git Concourse resource to make the source available
 
-# do the actual job
-${CWDIR}/ic_gpdb.bash
+    ssh $gpdb_master_alias "bash -c \"\
+        source /usr/local/greenplum-db-devel/greenplum_path.sh; \
+        export PGPORT=5432; \
+        export MASTER_DATA_DIRECTORY=/data/master/gpseg-1; \
+
+        cd /home/gpadmin/gpdb_src; \
+        ./configure --prefix=/usr/local/greenplum-db-devel --without-zlib --without-rt --without-libcurl --without-libedit-preferred --without-docdir --without-PACKAGE --without-readline --disable-gpcloud --disable-gpfdist --disable-orca ${CONFIGURE_FLAGS}; \
+        cd /home/gpadmin/gpdb_src; \
+        make installcheck-resgroup; \
+    \""
+}
+prepare_cgroups ccp-${CLUSTER_NAME}-0
+prepare_cgroups ccp-${CLUSTER_NAME}-1
+run_resgroup_test mdw
