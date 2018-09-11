tvesely/yugabyte-db

Add new replication mode synchronous_commit = 'remote_apply'.
In this mode, the master waits for the transaction to be applied on
the remote side, not just written to disk.  That means that you can
count on a transaction started on the standby to see all commits
previously acknowledged by the master.

To make this work, the standby sends a reply after replaying each
commit record generated with synchronous_commit >= 'remote_apply'.
This introduces a small inefficiency: the extra replies will be sent
even by standbys that aren't the current synchronous standby.  But
previously-existing synchronous_commit levels make no attempt at all
to optimize which replies are sent based on what the primary cares
about, so this is no worse, and at least avoids any extra replies for
people not using the feature at all.

Thomas Munro, reviewed by Michael Paquier and by me.  Some additional
tweaks by me.

src/postgres/src/backend/access/transam/xact.c
@@ -1324,7 +1324,7 @@ RecordTransactionCommit(void)
 	 * in the procarray and continue to hold locks.
 	 */
 	if (wrote_xlog && markXidCommitted)
-		SyncRepWaitForLSN(XactLastRecEnd);
+		SyncRepWaitForLSN(XactLastRecEnd, true);
 
 	/* remember end of last commit record */
 	XactLastCommitEnd = XactLastRecEnd;
@@ -5123,6 +5123,13 @@ XactLogCommitRecord(TimestampTz commit_time,
 		xl_xinfo.xinfo |= XACT_COMPLETION_FORCE_SYNC_COMMIT;
 
 	/*
+	 * Check if the caller would like to ask standbys for immediate feedback
+	 * once this commit is applied.
+	 */
+	if (synchronous_commit >= SYNCHRONOUS_COMMIT_REMOTE_APPLY)
+		xl_xinfo.xinfo |= XACT_COMPLETION_APPLY_FEEDBACK;
+
+	/*
 	 * Relcache invalidations requires information about the current database
 	 * and so does logical decoding.
 	 */
@@ -5459,6 +5466,13 @@ xact_redo_commit(xl_xact_parsed_commit *parsed,
 	if (XactCompletionForceSyncCommit(parsed->xinfo))
 		XLogFlush(lsn);
 
+	/*
+	 * If asked by the primary (because someone is waiting for a synchronous
+	 * commit = remote_apply), we will need to ask walreceiver to send a
+	 * reply immediately.
+	 */
+	if (XactCompletionApplyFeedback(parsed->xinfo))
+		XLogRequestWalReceiverReply();
 }
 
 /*

xiong-gang/gpdb

src/backend/access/transam/xact.c
@@ -1316,6 +1316,12 @@ RecordTransactionCommit(void)
 				xlrec.xinfo |= XACT_COMPLETION_UPDATE_RELCACHE_FILE;
 			if (forceSyncCommit)
 				xlrec.xinfo |= XACT_COMPLETION_FORCE_SYNC_COMMIT;
+			/*
+			 * Check if the caller would like to ask standbys for immediate feedback
+			 * once this commit is applied.
+			 */
+			if (synchronous_commit >= SYNCHRONOUS_COMMIT_REMOTE_APPLY)
+				xlrec.xinfo |= XACT_COMPLETION_APPLY_FEEDBACK;
 
 			xlrec.dbId = MyDatabaseId;
 			xlrec.tsId = MyDatabaseTableSpace;
@@ -1542,7 +1548,7 @@ RecordTransactionCommit(void)
 	if (markXidCommitted || isDtxPrepared)
 	{
 		Assert(recptr.xrecoff != 0);
-		SyncRepWaitForLSN(recptr);
+		SyncRepWaitForLSN(recptr, true);
 	}
 
 	/* Compute latestXid while we have the child XIDs handy */
@@ -5874,6 +5880,13 @@ xact_redo_commit_internal(TransactionId xid, XLogRecPtr lsn,
 	if (XactCompletionForceSyncCommit(xinfo))
 		XLogFlush(lsn);
 
+	/*
+	 * If asked by the primary (because someone is waiting for a synchronous
+	 * commit = remote_apply), we will need to ask walreceiver to send a
+	 * reply immediately.
+	 */
+	if (XactCompletionApplyFeedback(parsed->xinfo))
+		XLogRequestWalReceiverReply();
 }
 
 /*