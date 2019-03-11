63c6dea37bca643ec3cb2f05d099c89dc0e57422
Melanie Plageman <melanieplageman@gmail.com>
2019-03-11 16:19:20 -0700

    Don't squelch in our case

--- src/backend/executor/nodeNestloop.c
+++ src/backend/executor/nodeNestloop.c
@@ -359,7 +359,7 @@ ExecNestLoop(NestLoopState *node)
 		 * clog up the pipeline with our never-to-be-consumed
 		 * data.
 		 */
-		ExecSquelchNode((PlanState *) node);
+		//ExecSquelchNode((PlanState *) node);
 	}
 
 	return result;
